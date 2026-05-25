b76f0362dd02ad333937d7daec62605961c4d544
KLasareishvili <k_lasareishvili@yahoo.com>
2026-05-25 22:05:04 +0400

    Assignment 6

---
+++ Assignment6/main.js
@@ -0,0 +1,61 @@
+// 1) რა თანმიმდევრობით დაილოგება შემდეგი ინსტრუქციები: 
+// console.log("1");
+// setTimeout(() => console.log("2"), 100);
+// setTimeout(() => console.log("3"), 0);
+// Promise.resolve().then(() => console.log("4"));
+// console.log("5");
+
+// console.log("Answer1: 1 5 4 3 2");
+
+
+// 2) რა თანმიმდევრობით დაილოგება შემდეგი ინსტრუქციები: 
+// console.log("1");
+// setTimeout(() => console.log("2"), 0);
+// Promise.resolve().then(() => {
+//   console.log("3");
+//   setTimeout(() => console.log("4"), 0);
+// });
+// console.log("5");
+
+// console.log("Answer1: 1 5 3 2 4");
+
+// 3) დაწერეთ სლიფ ფუნქცია რომელიც პარამეტრად მიიღებს მილიწამს და დაიძინებს, 
+// ანუ სისტემა გაჩერდება პარამეტრის მიხედვით. await sleep(1000) სადაც ამ ფუნცქიას გამოიყენებთ 1 წამი უნდა 
+// გაჩერდეს ხოლმე სისტემა, გაითვალისწინეთ await ით უნდა გააჩეროთ ანუ პრომისი უნდა დააბრუნოს ფუნქციამ
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+// 4) დაწერეთ ფუნცქია რომელიც პარამეტრად მიიღებს რიცხვს 1-დან 20-მდე თქვენი მიზანია ფუნცქიის შიგნით ფუნქციამ 
+// ყოველ 1 წამში რენდომ რიცხვი დააგენერიროს მანამ სანამ რენდომ დაგენერირებული რიცხვი არ დამეთხვევა პარამეტს, 
+// როგორც კი ისინი ერთმანეთს დაემთხვევა გააჩერეთ რენდომ რიცხვის დალოგვა.
+function logRandomNumbers(n) {
+    const interval = setInterval(() => {
+        const random = Math.floor(Math.random() * 20) + 1
+        console.log(random)
+
+        if(random === n) {
+            clearInterval(interval)
+        }
+    }, 1000)
+}
+
+// logRandomNumbers(7)
+
+// 5) დაწერეთ ფუნცქია რომელსაც გადაეცემა 2 პარამეტრი 
+// 1 - ნებისმიერი რიცხვი 2 - დროის ერთეული მილიწამებში, 
+// თქვენი მიზანია დალოგოთ რიცხვები ამ რიცხვიდან 0 მდე იმ დროის ინტერვალში რაც არის მეორე პარამეტრი და 0ზე გააჩეროთ.
+
+function countDown(n, ms) {
+    const interval = setInterval(() => {
+        console.log(n)
+        n -= 1
+        
+        if(n === 0) {
+            clearInterval(interval)
+        }
+    }, ms)
+}
